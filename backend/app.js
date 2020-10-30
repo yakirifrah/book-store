861c0c71ba7225680cab20c151297fc7fe05e394
yakir <yakirifrah19@gmail.com>
2020-10-30 15:59:25 +0200

    Version 1.0.0

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,9 @@ if (process.env.NODE_ENV === 'development') {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve('../', 'frontend', 'build', 'index.html'));
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
     });
   }
 }
