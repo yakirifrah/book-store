ba6b914b499f02a0b6d285f4a259ad5d385db003
yakir <yakirifrah19@gmail.com>
2020-10-30 16:09:35 +0200

    Change the app.js server

--- backend/app.js
+++ backend/app.js
@@ -16,15 +16,15 @@ app.use(cors());
 app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
+}
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(
         path.resolve(__dirname, '../frontend', 'build', 'index.html')
-      );
-    });
-  }
+    );
+  });
 }
 
 // ROUTES
